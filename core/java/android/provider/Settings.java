5a674f811388fa7a5bda736afad75c560f717434
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2012-11-27 14:09:18 -0500

    Port "Option to hide Adb notification icon (2/2)"
    Change-Id: I2a6c5eed646c4a2121890201de2a7b8cca7f1b1a

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6810,6 +6810,12 @@ public final class Settings {
         public static final String QS_DATA_ADVANCED = "qs_data_advanced";
 
         /**
+         * Whether to display the ADB notification.
+         * @hide
+         */
+        public static final String ADB_NOTIFY = "adb_notify";
+
+        /**
          * This are the settings to be backed up.
          *
          * NOTE: Settings are backed up and restored in the order they appear
