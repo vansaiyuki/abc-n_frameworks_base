c505010f730ec0429a4267352d00c78aeb1e446a
maxwen <max.weninger@gmail.com>
2016-10-21 10:17:56 +0200

    Hide QS on secure losckreen feature: hide QS also in the quick bar

--- packages/SystemUI/src/com/android/systemui/qs/QSContainer.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSContainer.java
@@ -61,6 +61,7 @@ public class QSContainer extends FrameLayout {
     private QSCustomizer mQSCustomizer;
     private NotificationPanelView mPanelView;
     private boolean mListening;
+    private boolean mSecureExpandDisabled;
 
     public QSContainer(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -240,7 +241,7 @@ public class QSContainer extends FrameLayout {
         mQsExpansion = expansion;
         final float translationScaleY = expansion - 1;
         if (!mHeaderAnimating) {
-            setTranslationY(mKeyguardShowing ? (translationScaleY * mHeader.getHeight())
+            setTranslationY((mKeyguardShowing || mSecureExpandDisabled) ? (translationScaleY * mHeader.getHeight())
                     : headerTranslation);
         }
         mHeader.setExpansion(mKeyguardShowing ? 1 : expansion);
@@ -257,6 +258,9 @@ public class QSContainer extends FrameLayout {
     }
 
     public void animateHeaderSlidingIn(long delay) {
+        if (mSecureExpandDisabled) {
+            return;
+        }
         if (DEBUG) Log.d(TAG, "animateHeaderSlidingIn");
         // If the QS is already expanded we don't need to slide in the header as it's already
         // visible.
@@ -268,6 +272,9 @@ public class QSContainer extends FrameLayout {
     }
 
     public void animateHeaderSlidingOut() {
+        if (mSecureExpandDisabled) {
+            return;
+        }
         if (DEBUG) Log.d(TAG, "animateHeaderSlidingOut");
         mHeaderAnimating = true;
         animate().y(-mHeader.getHeight())
@@ -312,6 +319,11 @@ public class QSContainer extends FrameLayout {
     };
 
     public int getQsMinExpansionHeight() {
-        return mHeader.getHeight();
+        return mSecureExpandDisabled ? 0 : mHeader.getHeight();
+    }
+
+    public void setSecureExpandDisabled(boolean value) {
+        if (DEBUG) Log.d(TAG, "setSecureExpandDisabled " + value);
+        mSecureExpandDisabled = value;
     }
 }
--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
@@ -1067,6 +1067,7 @@ public class NotificationPanelView extends PanelView implements
         mStatusBarState = statusBarState;
         mKeyguardShowing = keyguardShowing;
         mQsContainer.setKeyguardShowing(mKeyguardShowing);
+        mQsContainer.setSecureExpandDisabled(isQsSecureExpandDisabled());
 
         if (goingToFullShade || (oldState == StatusBarState.KEYGUARD
                 && statusBarState == StatusBarState.SHADE_LOCKED)) {
