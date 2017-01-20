6d9be998775fcb311f9388926b5653680760ad76
ezio84 <brabus84@gmail.com>
2017-01-20 17:49:12 +0100

    Add multiuser for some features

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -1380,8 +1380,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                 }
                 KeyguardManager km = (KeyguardManager) mContext.getSystemService(Context.KEYGUARD_SERVICE);
                 boolean locked = km.inKeyguardRestrictedInputMode();
-                boolean globalActionsOnLockScreen = Settings.System.getInt(
-                        mContext.getContentResolver(), Settings.System.POWER_MENU_LOCKSCREEN, 1) == 1;
+                boolean globalActionsOnLockScreen = Settings.System.getIntForUser(
+                        mContext.getContentResolver(), Settings.System.POWER_MENU_LOCKSCREEN, 1, UserHandle.USER_CURRENT) == 1;
                 if (locked && !globalActionsOnLockScreen) {
                     behavior = LONG_PRESS_POWER_NOTHING;
                 } else {
