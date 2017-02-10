e73ead10f43549a3a1730e3500aa209c0c51e499
yoinx <joseph.schubert@gmail.com>
2017-02-09 19:50:34 -0500

    Add High brightness mode QS Tile
    - This is a conditional tile and will only show if the proper
    support is found in FWB and device tree.
    
    NOTE:
    Proof of Concept tile. This should actually be incorporated as
    a conditional third state in the existing brightness tile to make
    it more apparent to users that the manual and auto brightness
    modes are overriden by this. Essentialy to indicate that it is
    a third mode.
    
    *DU Edits*
    - Cleaned up tile some
    - Use vector for icons
    - Added toast when HSB is active to hopefully prevent bogus bugs
    
    @nathanchance edits: I don't overlay the tile config so it's
    built into this commit
    
    Change-Id: I886d72b0e40bebeb3ed70bb6956ddd104842b7ed

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
@@ -56,6 +56,7 @@ import com.android.systemui.qs.tiles.ColorInversionTile;
 import com.android.systemui.qs.tiles.DataSaverTile;
 import com.android.systemui.qs.tiles.DndTile;
 import com.android.systemui.qs.tiles.FlashlightTile;
+import com.android.systemui.qs.tiles.HighBrightnessTile;
 import com.android.systemui.qs.tiles.HotspotTile;
 import com.android.systemui.qs.tiles.IntentTile;
 import com.android.systemui.qs.tiles.LocationTile;
@@ -91,7 +92,6 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
-/** Platform implementation of the quick settings tile host **/
 public class QSTileHost implements QSTile.Host, Tunable {
     private static final String TAG = "QSTileHost";
     private static final boolean DEBUG = Log.isLoggable(TAG, Log.DEBUG);
@@ -118,6 +118,7 @@ public class QSTileHost implements QSTile.Host, Tunable {
     private final BatteryController mBattery;
     private final StatusBarIconController mIconController;
     private final TileServices mServices;
+    private final boolean mHighBrightnessSupported;
 
     private final List<Callback> mCallbacks = new ArrayList<>();
     private final AutoTileManager mAutoTiles;
@@ -127,14 +128,15 @@ public class QSTileHost implements QSTile.Host, Tunable {
     private int mCurrentUser;
 
     public QSTileHost(Context context, PhoneStatusBar statusBar,
-            BluetoothController bluetooth, LocationController location,
-            RotationLockController rotation, NetworkController network,
-            ZenModeController zen, HotspotController hotspot,
-            CastController cast, FlashlightController flashlight,
-            UserSwitcherController userSwitcher, UserInfoController userInfo,
-            KeyguardMonitor keyguard, SecurityController security,
-            BatteryController battery, StatusBarIconController iconController,
-            NextAlarmController nextAlarmController) {
+                      BluetoothController bluetooth, LocationController location,
+                      RotationLockController rotation, NetworkController network,
+                      ZenModeController zen, HotspotController hotspot,
+                      CastController cast, FlashlightController flashlight,
+                      UserSwitcherController userSwitcher, UserInfoController userInfo,
+                      KeyguardMonitor keyguard, SecurityController security,
+                      BatteryController battery, StatusBarIconController iconController,
+                      NextAlarmController nextAlarmController) {
+
         mContext = context;
         mStatusBar = statusBar;
         mBluetooth = bluetooth;
@@ -153,6 +155,7 @@ public class QSTileHost implements QSTile.Host, Tunable {
         mIconController = iconController;
         mNextAlarmController = nextAlarmController;
         mProfileController = new ManagedProfileController(this);
+        mHighBrightnessSupported = mContext.getResources().getBoolean(com.android.internal.R.bool.config_supportHighBrightness);
 
         final HandlerThread ht = new HandlerThread(QSTileHost.class.getSimpleName(),
                 Process.THREAD_PRIORITY_BACKGROUND);
@@ -453,6 +456,7 @@ public class QSTileHost implements QSTile.Host, Tunable {
         else if (tileSpec.equals("navigation_bar")) return new NavigationBarTile(this);
         else if (tileSpec.equals("ambient")) return new AmbientLiftToWakeTile(this);
         else if (tileSpec.equals("nfc")) return new NfcTile(this);
+        else if (tileSpec.equals("high_brightness") && mHighBrightnessSupported) return new HighBrightnessTile(this);
         // Intent tiles.
         else if (tileSpec.startsWith(IntentTile.PREFIX)) return IntentTile.create(this,tileSpec);
         else if (tileSpec.startsWith(CustomTile.PREFIX)) return CustomTile.create(this,tileSpec);
